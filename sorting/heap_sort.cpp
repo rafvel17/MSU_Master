f0e7055a100b14306eb01d1c73fa7be87518f4a5
rafvel17 <rafvel17@ya.ru>
2023-11-15 18:19:47 +0300

    segment sorting modification

--- sorting/heap_sort.cpp
+++ sorting/heap_sort.cpp
@@ -4,17 +4,22 @@
 using namespace std;
 
 void swap (vector<double>&, int, int);
-bool heapify (vector<double>&, int);
+void heapify (vector<double>&, int, size_t);
 void maxHeapBuild (vector<double>&, size_t);
 void maxHeapify (vector<double>&, size_t);
-void heapSort (vector<double>::iterator first, vector<double>::iterator last);
+void heapSort (vector<double> &, vector<double>::iterator first, vector<double>::iterator last);
 vector<double> getArray ();
 void printArray (vector<double> const &); 
 
 int main ()
 {
+    cout << "Insert an array" << endl;
     vector<double> array = getArray();
-    heapSort(array.begin(), array.end());
+    cout << "Choose the segment borders" << endl;
+    int it1 = 0;
+    int it2 = 0;
+    cin >> it1 >> it2;
+    heapSort(array, array.begin() + it1 - 1, array.begin() + it2 - 1);
     printArray(array);
     return 0;
 }
@@ -30,43 +35,50 @@ void swap (vector<double>& arr, int i, int j)
     arr[j] = tmp;
 } 
 
-bool heapify (std::vector<double>& arr, int i)
+void heapify (std::vector<double>& arr, int i, size_t heapSize)
 {
     double max = arr.at(i);
-    double left = arr.at(2*i + 1);
-    double right = arr.at(2*i + 2);
-    if (max < left || max < right)
+    if (2*i + 1 < heapSize)
     {
-        if (left <= right)
-            swap(arr, i, 2*i + 2);
-        else 
-            swap(arr, i, 2*i + 1);
-        return true;
+        double left = arr.at(2*i + 1);
+        if (2*i + 2 < heapSize)
+        {
+            double right = arr.at(2*i + 2);
+            if (max < left || max < right)
+            {
+                if (left <= right)
+                swap(arr, i, 2*i + 2);
+            else 
+                swap(arr, i, 2*i + 1);
+            }
+        }
+        else if (max < left)
+            {
+                swap(arr, i, 2*i + 1);
+            }
     }
-    else
-        return false;
+    
 }
 
 void maxHeapBuild (std::vector<double>& arr, size_t heapSize)
 {
     for (int i = heapSize / 2 - 1; i >= 0; --i)
-     	heapify(arr, i);
+     	heapify(arr, i, heapSize);
 }
 
 void maxHeapify(vector<double>& arr, size_t heapSize)
 {
     int i = 0;
-    bool flag = true;
     do 
     {
-        flag = heapify(arr, i);
+        heapify(arr, i, heapSize);
         i++;
     }
-    while ((2*i + 1 < heapSize) && flag == true);
+    while (2*i + 1 < heapSize);
 }
 
 
-void heapSort(vector<double>::iterator first, vector<double>::iterator last)
+void heapSort(vector<double> & arr, vector<double>::iterator first, vector<double>::iterator last)
 {
     std::vector<double> v;
     for (int i = 0; i <= (last - first); ++i)
@@ -83,9 +95,9 @@ void heapSort(vector<double>::iterator first, vector<double>::iterator last)
     std::vector<double>::iterator iter = v.begin();
     for (int i = 0; i <= (last - first); ++i)
     {
-        first + i = iter + i;
+        arr[first-arr.begin() + i] = v.at(i);
     }
-
+    
 }
 
 vector<double> getArray ()
